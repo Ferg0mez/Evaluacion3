631aec505cfc549787e90b3e11e40ef8403cc3e6
Ferg0mez <38075233+Ferg0mez@users.noreply.github.com>
2018-11-29 01:14:50 -0300

    Add files via upload

---
+++ Perris/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns=[
+    url(r'^$',views.index),
+    url(r'^index/$',views.index,name="inicio"),
+    url(r'^Usuarios/$',views.gestionarUsuarios,name="gestionarUsuarios"),
+    url(r'^registro/$',views.registro,name="registro"),
+    url(r'^Mascota/$',views.MascotaCreate.as_view(),name="MascotaNueva"),
+    url(r'^MascotaLista',views.MascotaList.as_view(),name="listaMascota"),
+    url(r'^Editar/(?P<pk>\d+)/$',views.MascotaUpdate.as_view(),name="editarMascota"),
+    url(r'^Eliminar/(?P<pk>\d+)/$',views.MascotaDelete.as_view(),name="eliminarMascota"),
+    url(r'^login/$',views.ingresar,name="login"),
+    url(r'^oauth/', include('social_django.urls', namespace='social')),
+    url(r'^salir/$',views.salir,name="logout")
+
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Para poder hacer que la foto cargue en la pagina.
+
